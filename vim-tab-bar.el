b0846f214602a96a0c812029b10494749eb373a2
James Cherti <60946298+jamescherti@users.noreply.github.com>
2024-07-14 18:13:04 -0400

    Release: 1.0.6

--- vim-tab-bar.el
+++ vim-tab-bar.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2024 James Cherti | https://www.jamescherti.com/contact/
 
 ;; Author: James Cherti
-;; Version: 1.0.5
+;; Version: 1.0.6
 ;; URL: https://github.com/jamescherti/vim-tab-bar.el
 ;; Keywords: frames
 ;; Package-Requires: ((emacs "28.1"))
